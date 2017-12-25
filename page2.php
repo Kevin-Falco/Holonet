e1ee91adb4f121fde2c3f0a711113d2ec7459c66
f16000576 <kevin.falco@etu.univ-amu.fr>
2017-12-25 22:56:14 +0100

    Ajout de bootstrap et de mise en forme du site

--- page2.php
+++ page2.php
@@ -7,8 +7,15 @@
     <meta charset="utf-8">
     <meta name="description" content="Projet PHP de 2e année réalisée par l\'équipe constituée d'Anthony, Théo, Léo, Hugo et Kévin">
     <meta name="keywords" content="iut, amu, php, 2e année, projet, info">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+
     <script src="js/toolbar.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 <body>
 <header>
