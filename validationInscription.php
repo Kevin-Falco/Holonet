f89680595b5c9711326de210b5ea64039e98bc99
anthony-jou <anthony.jou@etu.univ-amu.fr>
2018-01-09 10:24:09 +0100

    Ajoutdu formulaire d'inscription.

---
+++ validationInscription.php
@@ -0,0 +1,16 @@
+<?php
+    include 'utils.inc.php';
+
+    debut_page();
+?>
+    <header>
+        <!-- Vide -->
+    </header>
+    <?php barre_Navigation(); //__FILE__?>
+
+    <label>Code d'activation</label><br/>
+    <input type="number"  name="code"/><br/>
+
+<?php
+    fin_page();
+?>
