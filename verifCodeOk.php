f60b6049c94a91494f152df725a83e72b9e64004
Emiya Shirou <anthony.jou@etu.univ-amu.fr>
2018-01-14 15:02:53 +0100

    Ajout d'une page verifCodeOk

---
+++ verifCodeOk.php
@@ -0,0 +1,16 @@
+<?php
+    include 'utils.inc.php';
+
+    session_start();
+    debut_page();
+?>
+
+    <header>
+        <!-- Vide -->
+    </header>
+
+    <?php barre_Navigation(2); //__FILE__?>
+
+    <label>Vérification effectuée √</label><br/>
+    <a href="index.php">Retourner à l'accueil</a>
+
