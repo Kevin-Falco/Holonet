335c76a9e2044dd4fb8e41a8e7f20ed61ba438f6
Hugo Fasone <hugofasone@outlook.fr>
2018-01-26 20:13:08 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,44 +1,45 @@
 <?php
-    include 'utils.inc.php';
+include 'utils.inc.php';
+include 'trad.php';
 
-    debut_page();
+debut_page();
 
-    if ($_GET['step'] == 'ERROR_MDP')
-    {
-        echo 'Erreur de mot de passe.';
-    }
-    else if ($_GET['step'] == 'ERROR_EMAIL')
-    {
-        echo 'Erreur, l\'e-mail n\'existe pas.';
-    }
-    else if ($_GET['step'] == 'VALIDATION')
-    {
-        echo 'Erreur, e-mail non-validée.';
-    }
+if ($_GET['step'] == 'ERROR_MDP')
+{
+    echo 'Erreur de mot de passe.';
+}
+else if ($_GET['step'] == 'ERROR_EMAIL')
+{
+    echo 'Erreur, l\'e-mail n\'existe pas.';
+}
+else if ($_GET['step'] == 'VALIDATION')
+{
+    echo 'Erreur, e-mail non-validée.';
+}
 ?>
 
     <header>
         <!-- Vide -->
     </header>
-    <?php barre_Navigation(3); //__FILE__?>
+<?php barre_Navigation(3); //__FILE__?>
 
-    <label class="titre">Connexion</label>
+    <label class="titre"> <?php echo gettext('Connexion') ?> </label>
 
 
     <form action="testConnexion.php" method="post">
 
-        <label>Email</label><br/>
+        <label><?php echo gettext('Email') ?></label><br/>
         <input type="text" name="email"><br/>
 
-        <label>Mot de passe</label><br/>
+        <label><?php echo gettext('Mot de passe') ?></label><br/>
         <input type="password" name="mdp"/><br/>
 
         <input type="submit" name="action" value="connexion"/><br/>
 
     </form>
-    <a href="mdp_oublie.php">Mot de passe oublié ?</a>
-    <a href="inscription.php">S'inscrire</a>
+    <a href="mdp_oublie.php"><?php echo gettext('Mot de passe oublié ?') ?></a>
+    <a href="inscription.php"><?php echo gettext('S\'inscrire') ?></a>
 
 <?php
-    fin_page();
+fin_page();
 ?>
