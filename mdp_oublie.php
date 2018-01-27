12afc4e7eeeaf453d47889b4dab6151995dc555d
Hugo Fasone <hugofasone@outlook.fr>
2018-01-27 21:26:11 +0100

    Update mdp_oublie.php

--- mdp_oublie.php
+++ mdp_oublie.php
@@ -6,13 +6,13 @@
     debut_page();
 
     if(isset($_GET['step']) && $_GET['step'] == 'ERROR_EMAIL') {
-        echo 'E-mail non valide. Réessayez. <br/>';
+        echo gettext('E-mail non valide. Réessayez.') . ' <br/>';
     }
 
     barre_Navigation(4)
 ?>
 <div class="Changement">
-    Entrez votre adresse e-mail. Si vous êtes inscrit vous recevrez un nouveau mot de passe via cette adresse.
+    <?= gettext('Entrez votre adresse e-mail. Si vous êtes inscrit vous recevrez un nouveau mot de passe via cette adresse.')?>
     <form action="mail_mdp.php" method="post">
         <label>Email</label><br/>
         <input type="email" name="email"/><br/>
