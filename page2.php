88f75ed2a2e643596700b382988e6693c14f9548
Hugo Fasone <hugofasone@outlook.fr>
2018-01-27 21:28:31 +0100

    Update page2.php

--- page2.php
+++ page2.php
@@ -8,7 +8,7 @@
     barre_Navigation(3);
 ?>
 <div class="article">
-    Voici la page 2. Elle n'a aucune spécificité donc envoyez nous vos idées.
+    <?= gettext('Voici la page 2. Elle n\'a aucune spécificité donc envoyez nous vos idées.')?>
 </div>
 <?php
     fin_page();
