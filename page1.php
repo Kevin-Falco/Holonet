809632fc730eaf6ea3972653fe7fed3448b34eb5
Hugo Fasone <hugofasone@outlook.fr>
2018-01-27 21:28:06 +0100

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,30 +1,31 @@
 <?php
-include 'utils.inc.php';
 include 'trad.php';
+include 'utils.inc.php';
 
 debut_page();
 
 
 ?>
-    <header>
-        <!-- Vide -->
-    </header>
+<header>
+    <!-- Vide -->
+</header>
 
 <?php barre_Navigation(2); //__FILE__?>
 
-    <div class="article">
-        <?= 'La page 1 où on rigole ienb';?>
-    </div>
+<div class="article">
+    <?= gettext('La page 1 où on rigole ienb');?>
+</div>
 
-    <form action="page1trad.php" method="post">
-        Mot à traduire : <br>
-        <input type="text" name="mot"><br>
-        Langue : <select name="lang" id="">
-            <option value="francais"> Francais </option>
-            <option value="anglais"> Anglais </option>
-        </select><br>
-        <input type="submit" name="action" value="Traduire"><br>
-    </form>
+<form action="page1trad.php" method="post">
+    <?= gettext('Mot à traduire : ')?><br>
+    <input type="text" name="mot"><br>
+    <?= gettext('Langue : ')?><select name="lang" id="">
+        <option value="francais"> <?= gettext('Francais')?> </option>
+        <option value="anglais"> <?= gettext('Anglais')?> </option>
+    </select><br>
+    <input type="hidden" name="action" value="Traduire"><br>
+    <input type="submit" value=<?= gettext("Traduire")?>><br>
+</form>
 
 <?php
 if(isset($_SESSION['trad_sentence']))
@@ -32,12 +33,12 @@ if(isset($_SESSION['trad_sentence']))
 if(isset($_SESSION['non_connecté']))
     echo $_SESSION['non_connecté']; $_SESSION['non_connecté'] = '';
 
-    if (isset($_SESSION['categorie']) &&
-        ($_SESSION['categorie'] == 'premium' || $_SESSION['categorie'] == 'traducteur' || $_SESSION['categorie'] == 'admin' )) {
-        echo '<a href="pagepremium.php"><br>Page Premium</a><br>';
-        if($_SESSION['categorie'] == 'traducteur' || $_SESSION['categorie'] == 'admin')
-            echo '<a href="pagetraducteur.php">Page Traducteur</a><br>';
+if (isset($_SESSION['categorie']) &&
+    ($_SESSION['categorie'] == 'premium' || $_SESSION['categorie'] == 'traducteur' || $_SESSION['categorie'] == 'admin' )) {
+    echo '<a href="pagepremium.php"><br>' . gettext('Page Premium') . '</a><br>';
+    if($_SESSION['categorie'] == 'traducteur' || $_SESSION['categorie'] == 'admin')
+        echo '<a href="pagetraducteur.php">' . gettext('Page Traducteur') . '</a><br>';
 
-    }
-    fin_page();
+}
+fin_page();
 ?>
